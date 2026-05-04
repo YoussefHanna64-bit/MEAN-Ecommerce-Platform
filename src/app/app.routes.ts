534f804386b2235084f165803f82d1a0e588bdd2
Ali Azouz <aliazouz.808@gmail.com>
2026-05-05 00:11:39 +0300

    Adding Payment Gateway

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,38 +4,43 @@ import { Signup } from './components/signup/signup';
 import { Main } from './components/main/main';
 import { Home } from './components/home/home';
 import { AboutUs } from './components/about-us/about-us';
+import { PaymentComponent } from './components/payment/payment';
 
 export const routes: Routes = [
-    {
-        path: "",
-        redirectTo: "login",
-        pathMatch: "full"
-    },
-    {
-        path:"login",
-        component: Login
-    },
-    {
-        path:"signup",
-        component: Signup
-    },
-    {
-        path: "main",
-        component: Main,
-        children: [
-            {
-                path:"",
-                redirectTo:"home",
-                pathMatch:'full'
-            },
-            {
-                path:"home",
-                component: Home
-            },
-            {
-                path:"about",
-                component: AboutUs
-            }
-        ]
-    }
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: 'signup',
+    component: Signup,
+  },
+  {
+    path: 'main',
+    component: Main,
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        component: Home,
+      },
+      {
+        path: 'about',
+        component: AboutUs,
+      },
+      {
+        path: 'payment',
+        component: PaymentComponent,
+      },
+    ],
+  },
 ];
---
+++ src/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  stripe: {
+    publicKey:
+      'pk_test_51TRzDk2NbYu1cacqGwrHNhUzRouNzsjIwNnC58nmRoOrDXFEJjvJVuXbF2gDfZwVwxh7UX7Y9Vb6ycoLEpZoHoxM00upuostv9',
+  },
+  apiUrl: 'http://localhost:5000',
+};
