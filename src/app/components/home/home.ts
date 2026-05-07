f4fa8abe3353c6f9e08dab55df969552fdfe3600
Ali Azouz <aliazouz.808@gmail.com>
2026-05-07 23:01:37 +0300

    Refactoring Home UI

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -5,16 +5,17 @@ import { Footer } from '../footer/footer';
 import { Product } from '../../models/productModel';
 import { ProductService } from '../../Services/productServices';
 import { Chatpot } from '../chatpot/chatpot';
+import { HeroSectionComponent } from '../hero-section/hero-section';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, Chatpot],
+  imports: [CommonModule, Chatpot, HeroSectionComponent],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home implements OnInit {
- products = signal<Product[]>([]);
- chatpotOpen = signal<boolean>(false);
+  products = signal<Product[]>([]);
+  chatpotOpen = signal<boolean>(false);
   productService = inject(ProductService);
 
   ngOnInit() {
@@ -22,16 +23,20 @@ export class Home implements OnInit {
   }
 
   toggleChatpot() {
-    this.chatpotOpen.update(value => !value);
+    this.chatpotOpen.update((value) => !value);
   }
 
+  onShopCollection() {
+    // scroll to products or navigate — up to you
+    document.querySelector('.product-card')?.scrollIntoView({ behavior: 'smooth' });
+  }
   loadProducts() {
     //console.log('Loading products');
     this.productService.getProducts().subscribe({
       next: (res) => {
         //console.log(res.data?.products);
-       this.products.set(res.data?.products!);
-       //console.log(this.products());
+        this.products.set(res.data?.products!);
+        //console.log(this.products());
       },
       error: (err) => console.error('Failed to load products', err),
     });
